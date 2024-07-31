4df9a4d31fb20a63c7b58d692f37300377b7be01
DEBASHIS GARAI <debashis1805@gmail.com>
2024-07-31 23:13:56 +0530

    signup and home page done

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import HomeNavbar from "./HomeNavbar";
 
+
 const Home = () => {
+  const nav = useNavigate();
   return (
     <>
-      <HomeNavbar />
-      <div>This is the home</div>
+      <div className="w-full h-screen">
+        <HomeNavbar />
+        <div className="w-[80%] m-auto">
+          {/* Signup-with-text-div */}
+          <div className="text-center max-w-[60%] mx-auto pt-[3rem] tracking-wider flex flex-col gap-[1rem] items-center">
+            <div className="text-black text-6xl leading-[5rem] font-bold">
+              Finally, get your <span className="text-[#f37e6c]">life</span> and{" "}
+              <span className="text-[#f37e6c]">work</span> organized.
+            </div>
+            <div className="text-black/50 leading-[2rem] text-2xl max-w-[70%] m-auto">
+              With the help of this task manager and to-do list app, simplify
+              your life.
+            </div>
+            <div className="w-[25%] relative mt-[1rem]">
+            <button className=" bg-[#ea5740] w-full py-[0.5rem] text-xl font-semibold hover:translate-y-[3px] rounded-md shadow-md hover:bg-[#ee523a] text-white"
+            onClick={() => nav('/signup')}>
+              Let&apos;s Explore
+            </button>
+            <div className="h-[3rem] w-full bg-[#efc199] absolute top-[3px] z-[-1] left-0 rounded-md shadow-md"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,106 @@
-const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa6";
 
-export default Signup
+const Signup = () => {
+  const [isVisible, setisVisible] = useState(false);
+
+  const handlePwdVisiblity = () => {
+    setisVisible(!isVisible);
+  };
+  const nav = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="h-screen w-gull bg-white">
+      <div className="w-[50%] h-full m-auto">
+        <nav className="text-2xl font-bold tracking-widest uppercase h-[4rem] w-full flex items-center">
+          taskify
+        </nav>
+        <div className="w-full h-[calc(100%-4rem)] flex flex-row pt-[7rem]">
+          {/* left-part -> with name, username and password */}
+          <div className="w-[50%] max-h-[calc(100% - 7rem)]">
+            <div className="w-full text-3xl font-bold tracking-wider">
+              Signup
+            </div>
+            <div className="w-full mt-[2rem]">
+              <form
+                action="submit"
+                className="flex flex-col gap-[0.5rem] px-[0.5rem]"
+                onClick={handleSubmit}
+              >
+                <div className="w-full border-[0.5px] rounded-md border-gray-300 flex flex-col px-[0.5rem] bg-white py-[0.3rem]">
+                  <label className="text-xs font-semibold tracking-wide">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="focus:outline-none focus:border-none"
+                    placeholder="Enter you name..."
+                  />
+                </div>
+                <div className="w-full border-[0.5px] rounded-md border-gray-300 flex flex-col px-[0.5rem] bg-white py-[0.3rem]">
+                  <label className="text-xs font-semibold tracking-wide">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    className="focus:outline-none focus:border-none"
+                    placeholder="Enter you email..."
+                  />
+                </div>
+                <div className="w-full border-[0.5px] rounded-md border-gray-300 flex flex-col px-[0.5rem] bg-white py-[0.3rem]">
+                  <label className="text-xs font-semibold tracking-wide">
+                    Password
+                  </label>
+                  <div className="w-full flex flex-row justify-between">
+                    <input
+                      type={isVisible ? "text" : "password"}
+                      className="focus:outline-none focus:border-none"
+                      placeholder="Enter you password..."
+                    />
+                    {!isVisible ? (
+                      <FaEyeSlash
+                        className="cursor-pointer"
+                        onClick={handlePwdVisiblity}
+                      />
+                    ) : (
+                      <FaEye
+                        className="cursor-pointer"
+                        onClick={handlePwdVisiblity}
+                      />
+                    )}
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="bg-[#ea5740] w-full py-[0.5rem] text-xl font-bold rounded-md shadow-md hover:bg-[#ee523a] active:translate-y-[1px] text-white mt-[1rem]"
+                >
+                  Sign up
+                </button>
+              </form>
+              <div className="w-full text-center text-xs mt-[2rem]">
+                Already signed up?{" "}
+                <span
+                  className="underline cursor-pointer"
+                  onClick={() => nav("/signin")}
+                >
+                  Go to sign in
+                </span>
+              </div>
+            </div>
+          </div>
+          {/* right-part -> with one image */}
+          <div className="w-[50%] max-h-[calc(100% - 7rem) bg-blue-400">
+            right
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
