6b117b5136548a93f7fab9ef9f12e4066ededc14
DEBASHIS GARAI <debashis1805@gmail.com>
2024-08-02 02:34:01 +0530

    main app structure done

--- client/src/components/Maincontent.jsx
+++ client/src/components/Maincontent.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
 
 const Maincontent = () => {
   return (
-    <div>Maincontent</div>
+    <div className='w-[60%] h-[450px] m-auto bg-red-300 mt-[2rem] rounded-xl py-[1rem] px-[1.5rem]'>
+        {/* for the top div */}
+        <div className="h-[80%] w-full flex flex-row">
+            {/* top-left div */}
+            <div className="w-[35%] h-full bg-green-300">
+
+            </div>
+            {/* top-right div */}
+            <div className="w-[65%] h-full  bg-purple-300">
+
+            </div>
+        </div>
+        {/* for the bottom div */}
+        <div className="h-[20%] w-full bg-blue-300">
+        </div>
+    </div>
   )
 }
 
