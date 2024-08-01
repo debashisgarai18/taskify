c73d5fe85067af3813756d9f160c161853d0022d
DEBASHIS GARAI <debashis1805@gmail.com>
2024-08-02 02:16:14 +0530

    sign out button added in the navigation bar

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -6,6 +6,9 @@ import LandingFooter from "./LandingFooter";
 
 const Landing = () => {
   const [name, setName] = useState("");
+  const [isActive, setisActive] = useState(false);
+
+
   const nav = useNavigate();
   const getUserData = async () => {
     const res = await fetch("http://localhost:3000/user/landing", {
@@ -31,12 +34,26 @@ const Landing = () => {
   const handleSignIn = () => {
     nav("/signin");
   };
+
+
   return (
+    
     <div className="w-full h-screen">
       {name ? (
         <div className="w-full h-screen relative">
-          <LandingNavbar />
-          <div></div>
+          <LandingNavbar updateActive = {setisActive} activeState = {isActive} />
+          <div
+          className={`py-[1rem] px-[1rem] w-[15rem] bg-[#F2EAEA] shadow-2xl absolute right-[19%] top-[3.7rem] rounded-md flex flex-col gap-[0.5rem] items-center justify-center ${
+            isActive ? "duration-300 ease-out transition-all translate-y-[20%] opacity-1" : "duration-300 ease-out transition-all opacity-0 -translate-y-0"
+          } `}
+        >
+          <button className="bg-[#f1ada3] w-[80%] px-[1rem] py-[0.2rem] rounded-md text-white font-semibold shadow-lg">Edit Profile</button>
+          <button className="bg-[#f37e6c] w-[80%] px-[1rem] py-[0.2rem] rounded-md text-white font-semibold shadow-lg">Sign Out</button>
+        </div>
+          <div className="w-[60%] m-auto pt-[2rem] text-center text-5xl tracking-wide font-bold">
+            Hello, <span className="uppercase text-[#f37e6c]">{name}</span>, <span className="text-[#585858]">Start planning today</span>
+          </div>
+          
           <Maincontent />
           <LandingFooter />
         </div>
