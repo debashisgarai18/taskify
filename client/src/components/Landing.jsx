4448e81d7768299703f29fb0fb1f3d7d39296efd
DEBASHIS GARAI <debashis1805@gmail.com>
2024-08-06 01:23:24 +0530

    updated the landing page navbar

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -41,7 +41,7 @@ const Landing = () => {
     <div className="w-full h-screen">
       {name ? (
         <div className="w-full h-screen relative">
-          <LandingNavbar updateActive = {setisActive} name = {name} />
+          <LandingNavbar updateActive = {setisActive}/>
           <div
           className={`py-[1rem] px-[1rem] w-[15rem] bg-[#F2EAEA] shadow-2xl absolute right-[19%] top-[3.7rem] rounded-md flex flex-col gap-[0.5rem] items-center justify-center ${
             isActive ? "duration-300 ease-out transition-all translate-y-[20%] opacity-1" : "duration-300 ease-out transition-all opacity-0 -translate-y-0"
@@ -50,7 +50,7 @@ const Landing = () => {
           <button className="bg-[#f1ada3] w-[80%] px-[1rem] py-[0.2rem] rounded-md text-white font-semibold shadow-lg">Edit Profile</button>
           <button className="bg-[#f37e6c] w-[80%] px-[1rem] py-[0.2rem] rounded-md text-white font-semibold shadow-lg">Sign Out</button>
         </div>
-          <div className="w-[60%] m-auto pt-[2rem] text-center text-5xl tracking-wide font-bold">
+          <div className="w-[60%] m-auto pt-[2rem] text-center text-5xl tracking-wide font-semibold">
             Hello, <span className="uppercase text-[#f37e6c]">{name}</span>, <span className="text-[#585858]">Start planning today</span>
           </div>
           
--- client/src/components/LandingNavbar.jsx
+++ client/src/components/LandingNavbar.jsx
@@ -1,24 +1,31 @@
 import userLogo from "../assets/jett_render_by_lynwulf_ddrsqbl-414w-2x.png";
 import PropTypes from "prop-types";
+import { IoMdArrowDropdown } from "react-icons/io";
 
-const LandingNavbar = ({ updateActive, name }) => {
+const LandingNavbar = ({ updateActive }) => {
   return (
     <div className="bg-white h-[4rem] w-full shadow-lg flex items-center justify-between">
       <div className="h-full w-[60%] m-auto flex items-center justify-between">
         <div className="text-2xl font-bold tracking-widest uppercase">
           Taskify
         </div>
-        <div className="flex items-center gap-[1rem]">
-          <div>{name}</div>
+        <div className="flex flex-col items-center gap-[5px] cursor-pointer"
+          onClick={() => updateActive((prev) => !prev)}
+        >
           <div
-            className="w-[3rem] h-[3rem] flex items-center rounded-[50%] bg-black overflow-hidden cursor-pointer"
-            onClick={() => updateActive((prev) => !prev)}
+            className="w-[2rem] h-[2rem] flex items-center rounded-[50%] bg-black overflow-hidden"
           >
             <img
               src={userLogo}
               className="h-full w-full object-contain"
               alt="userlogo"
             />
+          </div>
+          <div className="flex flex-row items-center">
+            <div className="text-xs">Me</div>
+            <div>
+              <IoMdArrowDropdown />
+            </div>
           </div>
         </div>
       </div>
@@ -28,7 +35,6 @@ const LandingNavbar = ({ updateActive, name }) => {
 
 LandingNavbar.propTypes = {
   updateActive: PropTypes.func,
-  name: PropTypes.string.isRequired,
 };
 
 export default LandingNavbar;
