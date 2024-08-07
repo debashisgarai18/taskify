fb6ae2addc5c282d7060cbacd30e8f4ca3fba2f8
DEBASHIS GARAI <debashis1805@gmail.com>
2024-08-07 15:44:49 +0530

    minor

--- client/src/components/Maincontent.jsx
+++ client/src/components/Maincontent.jsx
@@ -1,12 +1,15 @@
 
 const Maincontent = () => {
+  const date = new Date();
+  console.log(date);
+
   return (
     <div className='w-[60%] h-[450px] m-auto bg-red-300 mt-[2rem] rounded-xl py-[1rem] px-[1.5rem]'>
         {/* for the top div */}
         <div className="h-[80%] w-full flex flex-row">
             {/* top-left div */}
             <div className="w-[35%] h-full bg-green-300">
-
+            
             </div>
             {/* top-right div */}
             <div className="w-[65%] h-full  bg-purple-300">
