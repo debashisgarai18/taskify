78221743da8184c0e57f6a43986f819d06ca147c
DEBASHIS GARAI <debashis1805@gmail.com>
2024-12-05 17:05:30 +0530

    changed the database code

--- server/Database/index.js
+++ server/Database/index.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const { mongo_url } = require("../config");
 mongoose.connect(mongo_url);
 
+// defifing the taskSchema, which will accept the taskname and the description from the user
+const taskSchema = new mongoose.Schema({
+  taskName: String,
+  description: String,
+});
 
 // defining the userSchema, which will store the username, pass and the tasks of the specific user
 const userSchema = new mongoose.Schema({
@@ -12,11 +17,6 @@ const userSchema = new mongoose.Schema({
 });
 
 // todo : update the tasks schema with the start date, completed, priority stuff
-// defifing the taskSchema, which will accept the taskname and the description from the user
-const taskSchema = new mongoose.Schema({
-  taskName: String,
-  description: String,
-});
 
 const tasks = mongoose.model("Tasks", taskSchema);
 const users = mongoose.model("Users", userSchema);
